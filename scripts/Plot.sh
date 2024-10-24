52bc9ffbcbdcb99b8c058dd416e675fc354d827b
Xxxxxi <57949104+Gao-Xi@users.noreply.github.com>
2024-10-24 10:46:26 +0800

    Update Plot.sh

--- scripts/Plot.sh
+++ scripts/Plot.sh
@@ -2,7 +2,7 @@
 
 
 #Convex_QCQP_RHS_100_50_50
-python plot.py --config .\configs\Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Condition1
-python plot.py --config .\configs\Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Condition2
-python plot.py --config .\configs\Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Residual
-python plot.py --config .\configs\Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Objective_values
+python plot.py --config ./configs/Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Condition1
+python plot.py --config ./configs/Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Condition2
+python plot.py --config ./configs/Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Residual
+python plot.py --config ./configs/Plot.yaml --prob_type Convex_QCQP_RHS --plot_type Objective_values
